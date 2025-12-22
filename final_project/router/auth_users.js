7addc0d7ff6d38342ffe24cc368b03c2770705fd
geriko70 <davideorlandino70@gmail.com>
2025-12-22 09:53:37 -0500

    authentication

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -10,7 +10,15 @@ const isValid = (username)=>{ //returns boolean
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+    let validuser=users.filter((user)=>{
+        return (user.username===username && user.password===password);
+    });
+
+    if(validusers.length>0){
+        return true;
+    }else{
+        return false;
+    }
 }
 
 //only registered users can login
